1e9fd68a66add3f3431156d1ef45304a774e726c
Hamim <hamimbasha@outlook.com>
2023-12-21 12:10:19 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let name = 'Hamim';
+console.log('my name is :' + name);
+
+let cost = 5 + 2*3 + 9;
+console.log(`Cost of food: $${cost}`);
+
+let Tax = (5 + 2*3 + 9 ) * 0.1;
+console.log('cost with tax: $' + Tax);
+
+let totalcost = cost + Tax;
+console.log('total cost: $' + totalcost);
+
