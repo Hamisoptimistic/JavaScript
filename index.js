2b35055d979d5a5db2886091f7dad3e22d3375fc
Hamim <hamimbasha@outlook.com>
2023-12-22 12:55:38 +0530

    comiiting for the sake

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-let name = 'Hamim';
-console.log('my name is :' + name);
+// let name = 'Hamim';
+// console.log('my name is :' + name);
 
-let cost = 5 + 2*3 + 9;
-console.log(`Cost of food: $${cost}`);
+// let cost = 5 + 2*3 + 9;
+// console.log(`Cost of food: $${cost}`);
 
-let Tax = (5 + 2*3 + 9 ) * 0.1;
-console.log('cost with tax: $' + Tax);
+// let Tax = (5 + 2*3 + 9 ) * 0.1;
+// console.log('cost with tax: $' + Tax);
 
-let totalcost = cost + Tax;
-console.log('total cost: $' + totalcost);
+// let totalcost = cost + Tax;
+// console.log('total cost: $' + totalcost);
 
+console.log(1/3);
